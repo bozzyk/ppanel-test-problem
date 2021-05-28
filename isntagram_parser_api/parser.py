8837b9454a043cb0fb229bbf025fe3d38533c65f
bozzyk44 <bozzyk44@gmail.com>
2021-05-28 17:12:26 +0300

    Update parser.py

--- isntagram_parser_api/parser.py
+++ isntagram_parser_api/parser.py
@@ -1,8 +1,8 @@
 from typing import List
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.common.action_chains import ActionChains
 from random import random
 from functools import partial
 import time
@@ -16,6 +16,15 @@ def slow_type(element: WebElement, text: str):
     for char in text:
         element.send_keys(char)
         time.sleep(random() * 0.3)  # reducing interval returned by random() from [0, 1) to [0, 0.3)
+
+
+def wait_and_perform_action(el: WebElement, function: str, argument: str, interval=2, retries=10):
+    while retries:
+        try:
+            return getattr(el, function)(argument)
+        except Exception:
+            retries -= 1
+            time.sleep(interval)
 
 
 def get_username(driver: webdriver.Chrome, user_id, username, password):
@@ -38,13 +47,13 @@ def get_username(driver: webdriver.Chrome, user_id, username, password):
 
 
 def log_in(driver: webdriver.Chrome, username, password):
-    username_form = driver.find_element_by_name('username')
+    username_form = wait_and_perform_action(driver, 'find_element_by_name', 'username')
     slow_type(username_form, username)
 
-    password_form = driver.find_element_by_name('password')
+    password_form = wait_and_perform_action(driver, 'find_element_by_name', 'password')
     slow_type(password_form, password)
 
-    submit_btn = driver.find_element_by_xpath('//button[@type="submit"]')
+    submit_btn = wait_and_perform_action(driver, 'find_element_by_xpath', '//button[@type="submit"]')
     submit_btn.click()
 
 
@@ -77,61 +86,46 @@ def get_posts(driver: webdriver.Chrome, recent=False):
     '''
     posts_a: List[WebElement] = driver.find_elements_by_xpath('//article/div[1]/div/div/div/a')
     posts_a = posts_a[:10] if recent else posts_a[10:20]
-    posts = list()
-    for a in posts_a:
-        post = {
-            'url': a.get_attribute('href'),
-            'preview': a.find_element_by_tag_name('img').get_attribute('src'),
-            }
-        a.click()
-        time.sleep(1.5)
+    
+    posts = [{
+        'url': a.get_attribute('href'),
+        'preview': a.find_element_by_tag_name('img').get_attribute('src')
+    } for a in posts_a]
 
-        likes_views: WebElement = driver.find_element_by_xpath('//section[2]/div')
+    for idx, post in enumerate(posts):
+        driver.get(post['url'])
+
+        likes_views: WebElement = wait_and_perform_action(driver, 'find_element_by_xpath', '//section[2]/div')
         if 'views' in likes_views.text:
             likes_views.find_element_by_tag_name('span').click()
             likes_views = driver.find_element_by_xpath('//section[2]/div')
-            post['likes'] = ''.join(likes_views.text.split('\n')[1].split(' ')[0].split(','))
+            posts[idx]['likes'] = ''.join(likes_views.text.split('\n')[1].split(' ')[0].split(','))
         else:
-            post['likes'] = ''.join(likes_views.text.split(' ')[0].split(','))
+            posts[idx]['likes'] = ''.join(likes_views.text.split(' ')[0].split(','))
 
-        post['likes'] = ''.join(driver.find_element_by_xpath('//section[2]/div/div/a/span').text.split(','))
-        driver.find_element_by_xpath('/html/body/div[last()]/div[last()]/button').click()
-
-        posts.append(post)
-    return posts
+        print(post)
+    return {'posts': posts}
 
 
-def main(host_username='', host_password='', username='cristiano', mode='profile'):
+def main(host_username='bozzyk44', host_password='Zsedcvbnm@#$', username='cristiano', mode='posts'):
     options = Options()
     # options.headless = True
     # options.add_argument(f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36')
     driver = webdriver.Chrome(options=options)
 
     driver.get('https://instagram.com')
-    time.sleep(1)
     log_in(driver, host_username, host_password)
-    time.sleep(4)
 
     # username = get_username(driver, user_id, host_username, host_password)
-    driver.get('https://instagram.com')
+    # driver.get('https://instagram.com')
 
-    if 'accounts/onetap/' in driver.current_url:  # case when instagram asks about saving Login info
-        not_now_btn = filter(lambda x: x.text == 'Not Now', driver.find_elements_by_tag_name('button')).__next__().click()
-        time.sleep(1)
-
-    notifications_btn : List[WebElement] = list(filter(lambda x: x.text == 'Not Now', driver.find_elements_by_tag_name('button')))
-    if notifications_btn:
-        notifications_btn[0].click()
-        time.sleep(1)
-
-    search_field : WebElement = driver.find_element_by_xpath('//input[@placeholder="Search"]')
+    search_field : WebElement = wait_and_perform_action(driver, 'find_element_by_xpath', '//input[@placeholder="Search"]')
     slow_type(search_field, username)
-    time.sleep(1)
-    user_field : WebElement = driver.find_element_by_xpath('//div[text() = "cristiano"]')
-    user_field.click()
-    time.sleep(1)
 
-    avatar_url = driver.find_element_by_xpath('//img').get_attribute('src')
+    user_field : WebElement = wait_and_perform_action(driver, 'find_element_by_xpath', '//div[text() = "cristiano"]')
+    user_field.click()
+
+    avatar_url = wait_and_perform_action(driver, 'find_element_by_xpath', '//img').get_attribute('src')
     
     if mode == 'profile':
         data = get_profile_info(driver)
